1de4b34b88b174a547bb33b66a65ebf1c44f6f01
fardeen shaikh <fardeenshaikh1418@gmail.com>
2025-04-28 18:07:25 +0530

    icon

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let body = document.querySelector("body");
+let form = document.querySelector(".container");
+let header = document.querySelector("#header");
 
 let loader = document.getElementById("loding");
 window.addEventListener("load", function () {
@@ -6,16 +8,12 @@ window.addEventListener("load", function () {
 });
 
 function login() {
-  let form = document.querySelector(".container");
-  let header = document.querySelector("#header");
   form.style.opacity = "1";
   header.style.opacity = "0.1";
   body.style.overflow = "hidden";
 }
 
 function login_btn() {
-  let form = document.querySelector(".container");
-  let header = document.querySelector("#header");
   form.style.opacity = "0";
   header.style.opacity = "1";
   body.style.overflow = "scroll";
